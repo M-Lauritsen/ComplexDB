f8fe3ee1eea79f5e4bca1de97d52e400a0210edd
Michael Lauritsen <i30288741@live.dk>
2019-07-26 11:30:50 +0200

    Add project files.

---
+++ ComplexDB/Migrations/20190724123944_inits.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ComplexDB.Migrations
+{
+    public partial class inits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
